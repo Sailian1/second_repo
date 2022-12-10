9302d466afe7a0cb62edd5c557a4a695326b2a29
mateuszz_wsb <mateuszo.85@gmail.com>
2022-12-10 11:31:20 +0100

    new commit

---
+++ os_time.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+print(os.getcwd())
+os.chdir('C:\\Users\\vdi-student\\Desktop\\nauka\\second_repo\\second_repo')
+
+
+os.mkdir('new_folder')
+os.rename('new_folder', 'new_folder2')
+os.rmdir('new_folder2')
+os.system('cmd /c "cd C://Users//kamil//Desktop”')
+time.sleep(5)
+
+
